0f4adff708959481293d006cf2f1a9115422c4c5
Jian Liu <onanying@126.com>
2017-01-19 11:33:23 +0800

    增加 isWeixinPC

--- appkit.js
+++ appkit.js
@@ -1,16 +1,27 @@
-;function Appkit() {
+;var Appkit = function() {
+
+    var obj = new Object();
 
     // 是否在微信浏览器中
-    this.isWeixin = function() {
+    obj.isWeixin = function() {
         var ua = navigator.userAgent.toLowerCase();
-        if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        if (ua.match(/micromessenger/i) == "micromessenger") {
+            return true;
+        }
+        return false;
+    };
+
+    // 是否在PC的微信浏览器中
+    obj.isWeixinPC = function() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.match(/micromessenger/i) == "micromessenger" &&  ua.match(/qqbrowser/i) == "qqbrowser") {
             return true;
         }
         return false;
     };
 
     // 是否在Android浏览器中
-    this.isAD = function() {
+    obj.isAD = function() {
         if (typeof(window.adwebkit) != "undefined") {
             return true;
         }
@@ -18,7 +29,7 @@
     };
 
     // 是否在iOS浏览器中
-    this.isIOS = function() {
+    obj.isIOS = function() {
         if (typeof(window.webkit) != "undefined") {
             return true;
         }
@@ -26,32 +37,34 @@
     };
 
     // 是否在App浏览器中
-    this.isApp = function() {
-        if (this.isAD() || this.isIOS()) {
+    obj.isApp = function() {
+        if (obj.isAD() || obj.isIOS()) {
             return true;
         }
         return false;
     };
 
     // JS调用App方法
-    this.callApp = function(method, json) {
+    obj.callApp = function(method, json) {
         var json = arguments[1] || null;
-        if (this.isAD()) {
-            this.callAD(method, json);
+        if (obj.isAD()) {
+            obj.callAD(method, json);
         }
-        if (this.isIOS()) {
-            this.callIOS(method, json);
+        if (obj.isIOS()) {
+            obj.callIOS(method, json);
         }
     };
 
     // JS调用Android方法
-    this.callAD = function(method, json) {
-        eval("window.adwebkit." + method + "(" + (json == null ? "": "'" + json + "'") + ");");
+    obj.callAD = function(method, json) {
+        eval("window.adwebkit." + method + "(" + (json == null ? "" : "'" + json + "'") + ");");
     };
 
     // JS调用iOS方法
-    this.callIOS = function(method, json) {
-        eval("window.webkit.messageHandlers." + method + ".postMessage(" + (json == null ? "null": json) + ");");
+    obj.callIOS = function(method, json) {
+        eval("window.webkit.messageHandlers." + method + ".postMessage(" + (json == null ? "null" : json) + ");");
     };
 
+    return obj;
 };
+var appkit = new Appkit();
